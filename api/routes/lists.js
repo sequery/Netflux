5d92921f3cace580f267148fe2bed27b991b7643
sequery <foreducation9800@gmail.com>
2021-11-05 15:30:28 +0500

    React FrontEnd

--- api/routes/lists.js
+++ api/routes/lists.js
@@ -90,8 +90,27 @@ router.get("/", verify, async (req, res) => {
                         }
                     }
                 ])
+            } else {
+                list = await List.aggregate([{
+                        $simple: {
+                            size: 10
+                        }
+                    },
+                    {
+                        $match: {
+                            type: typeQuery,
+                        }
+                    }
+                ])
             }
+        } else {
+            list = await List.aggregate([{
+                $simple: {
+                    size: 10
+                }
+            }])
         }
+        res.status(200).json(list);
     } catch (err) {
         res.status(500).json(err);
     }
